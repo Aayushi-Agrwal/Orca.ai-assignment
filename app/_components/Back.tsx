92f1399c1cce152ac5f97e3265fbf01e470f46a8
Aayushi-Agrwal <khushiagarwal01464@gmail.com>
2024-07-04 17:35:52 +0530

    feat: add path

--- app/_components/Back.tsx
+++ app/_components/Back.tsx
@@ -3,8 +3,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function BackButton() {
   return (
-    <div className="h-6 w-16 text-sm flex  gap-1 items-center justify-center rounded-md bg-gray-100 text-blue-900 cursor-default hover:bg-gray-200 transition-all">
-      <FontAwesomeIcon icon={faArrowLeft} size="sm" /> Back
+    <div className="flex text-sm text-blue-900 items-center gap-6">
+      <div className="h-6 w-16  flex gap-1 items-center justify-center rounded-md bg-gray-100  cursor-default hover:bg-gray-200 transition-all">
+        <FontAwesomeIcon icon={faArrowLeft} size="sm" /> Back
+      </div>
+      <p>Templates/Sales Effectiveness/ BANT</p>
     </div>
   );
 }
