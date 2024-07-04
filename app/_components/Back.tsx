59e086489c3bc0f7f79bf4c4a03320d06aec1b9a
Aayushi-Agrwal <khushiagarwal01464@gmail.com>
2024-07-04 17:32:03 +0530

    fix: dropdown styles "

--- app/_components/Back.tsx
+++ app/_components/Back.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function BackButton() {
   return (
-    <div className="h-6 w-16 text-sm flex  gap-1 items-center justify-center rounded-md bg-gray-200 text-gray-500">
+    <div className="h-6 w-16 text-sm flex  gap-1 items-center justify-center rounded-md bg-gray-100 text-blue-900 cursor-default hover:bg-gray-200 transition-all">
       <FontAwesomeIcon icon={faArrowLeft} size="sm" /> Back
     </div>
   );
