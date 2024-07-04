28e174248e00c084519a6b65b6d92b067a1f3dde
Aayushi-Agrwal <khushiagarwal01464@gmail.com>
2024-07-04 17:33:23 +0530

    fix: tabs padding

--- app/_components/Tabs.tsx
+++ app/_components/Tabs.tsx
@@ -10,7 +10,7 @@ const Tab = ({ tabs }: { tabs: Tab[] }) => {
   return (
     <div>
       <div className="border-b border-white ">
-        <nav className=" flex space-x-8" aria-label="Tabs">
+        <nav className="flex space-x-8" aria-label="Tabs">
           {tabs.map((tab, index) => (
             <button
               key={tab.name}
@@ -19,7 +19,7 @@ const Tab = ({ tabs }: { tabs: Tab[] }) => {
                 activeTab === index
                   ? "border-indigo-500 text-indigo-600"
                   : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
-              } whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm`}
+              } whitespace-nowrap py-2 px-1 border-b-2 font-medium text-sm`}
             >
               {tab.name}
             </button>
